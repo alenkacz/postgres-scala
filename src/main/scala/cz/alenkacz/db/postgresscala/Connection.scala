49c3df7630e36ae632ba571a0771c79ca035739a
Alena Varkockova <varkockova.a@gmail.com>
2017-02-03 19:19:00 +0100

    Changes by CR

--- src/main/scala/cz/alenkacz/db/postgresscala/Connection.scala
+++ src/main/scala/cz/alenkacz/db/postgresscala/Connection.scala
@@ -3,11 +3,9 @@ package cz.alenkacz.db.postgresscala
 import scala.concurrent.Future
 
 trait Connection {
-    def isConnected: Boolean
-
-    def sendQuery[T](query: String, deserializer: Row => T): Future[Seq[T]]
-    def sendQuery[T](query: String): Future[Option[T]]
-    def sendPreparedStatement[T](query: String, values: Seq[Any] = List()): Future[Option[T]]
+    def query[T](query: String, deserializer: Row => T): Future[Seq[T]]
+    def queryValue[T](query: String): Future[Option[T]]
+    def sendPreparedStatementForValue[T](query: String, values: Seq[Any] = Seq.empty): Future[Option[T]]
     def sendPreparedStatement[T](query: String, values: Seq[Any], deserializer: Row => T): Future[Seq[T]]
     def sendPreparedStatement[T](query: String, deserializer: Row => T): Future[Seq[T]]
 }
