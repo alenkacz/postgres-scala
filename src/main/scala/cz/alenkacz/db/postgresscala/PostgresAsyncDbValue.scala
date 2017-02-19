4dc6050211a1d8452a28be036a8b7006196d4106
Alena Varkockova <varkockova.a@gmail.com>
2017-02-19 15:23:45 +0100

    Integration test for all supported data types

--- src/main/scala/cz/alenkacz/db/postgresscala/PostgresAsyncDbValue.scala
+++ src/main/scala/cz/alenkacz/db/postgresscala/PostgresAsyncDbValue.scala
@@ -1,7 +1,7 @@
 package cz.alenkacz.db.postgresscala
 import java.net.InetAddress
 import java.sql.Time
-import java.time.Instant
+import java.time.{Instant, LocalTime}
 import java.util.UUID
 
 import org.joda.time.{DateTime, DateTimeZone, LocalDateTime}
@@ -13,7 +13,7 @@ class PostgresAsyncDbValue(value: Any) extends DbValue {
 
   override def int: Int = value.asInstanceOf[Int]
 
-  override def bigInt: BigInt = value.asInstanceOf[BigInt]
+  override def bigInt: Long = value.asInstanceOf[Long]
 
   override def double: Double = value.asInstanceOf[Double]
 
@@ -35,43 +35,46 @@ class PostgresAsyncDbValue(value: Any) extends DbValue {
     case _ => throw new IllegalArgumentException("Unsupported datetime type - expecting DateTime or LocalDatetime from joda library")
   }
 
-  override def time: Time = value.asInstanceOf[Time]
+  override def time: Time = {
+    val yodaTime = value.asInstanceOf[org.joda.time.LocalTime]
+    Time.valueOf(LocalTime.of(yodaTime.getHourOfDay, yodaTime.getMinuteOfHour, yodaTime.getSecondOfMinute))
+  }
 
-  override def bytes: Array[Byte] = value.asInstanceOf[ArrayBuffer[Byte]].toArray
+  override def bytes: Seq[Byte] = value.asInstanceOf[Array[Byte]].toList
 
   override def any: Any = value
 
   override def stringOpt: Option[String] = Option(string)
 
-  override def strings: Array[String] = value.asInstanceOf[ArrayBuffer[String]].toArray
+  override def strings: Seq[String] = value.asInstanceOf[ArrayBuffer[String]].toList
 
   override def intOpt: Option[Int] = Option(int)
 
-  override def ints: Array[Int] = value.asInstanceOf[ArrayBuffer[Int]].toArray
+  override def ints: Seq[Int] = value.asInstanceOf[ArrayBuffer[Int]].toList
 
-  override def bigIntOpt: Option[BigInt] = Option(bigInt)
+  override def bigIntOpt: Option[Long] = Option(bigInt)
 
-  override def bigInts: Array[BigInt] = value.asInstanceOf[ArrayBuffer[BigInt]].toArray
+  override def bigInts: Seq[Long] = value.asInstanceOf[ArrayBuffer[Long]].toList
 
   override def doubleOpt: Option[Double] = Option(double)
 
-  override def doubles: Array[Double] = value.asInstanceOf[ArrayBuffer[Double]].toArray
+  override def doubles: Seq[Double] = value.asInstanceOf[ArrayBuffer[Double]].toList
 
   override def floatOpt: Option[Float] = Option(float)
 
-  override def floats: Array[Float] = value.asInstanceOf[ArrayBuffer[Float]].toArray
+  override def floats: Seq[Float] = value.asInstanceOf[ArrayBuffer[Float]].toList
 
   override def longOpt: Option[Long] = Option(long)
 
-  override def longs: Array[Long] = value.asInstanceOf[ArrayBuffer[Long]].toArray
+  override def longs: Seq[Long] = value.asInstanceOf[ArrayBuffer[Long]].toList
 
   override def boolOpt: Option[Boolean] = Option(bool)
 
-  override def bools: Array[Boolean] = value.asInstanceOf[ArrayBuffer[Boolean]].toArray
+  override def bools: Seq[Boolean] = value.asInstanceOf[ArrayBuffer[Boolean]].toList
 
   override def shortOpt: Option[Short] = Option(short)
 
-  override def shorts: Array[Short] = value.asInstanceOf[ArrayBuffer[Short]].toArray
+  override def shorts: Seq[Short] = value.asInstanceOf[ArrayBuffer[Short]].toList
 
   override def inetAddressOpt: Option[InetAddress] = Option(inetAddress)
 
@@ -80,4 +83,8 @@ class PostgresAsyncDbValue(value: Any) extends DbValue {
   override def instantOpt: Option[Instant] = Option(instant)
 
   override def timeOpt: Option[Time] = Option(time)
+
+  override def inetAddresses: Seq[InetAddress] = value.asInstanceOf[ArrayBuffer[InetAddress]].toList
+
+  override def uuids: Seq[UUID] = value.asInstanceOf[ArrayBuffer[UUID]].toList
 }
