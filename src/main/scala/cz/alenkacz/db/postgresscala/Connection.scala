38428048b85cec56e65be8ec6d05d1f1be843865
Michal Augustyn <augustyn@avast.com>
2017-06-30 20:56:00 +0200

    support for transactions Closes #12

--- src/main/scala/cz/alenkacz/db/postgresscala/Connection.scala
+++ src/main/scala/cz/alenkacz/db/postgresscala/Connection.scala
@@ -1,6 +1,7 @@
 package cz.alenkacz.db.postgresscala
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.Failure
 
 trait Connection extends AutoCloseable {
     def query[T](query: String, deserializer: Row => T): Future[Seq[T]]
@@ -9,4 +10,17 @@ trait Connection extends AutoCloseable {
     def sendPreparedStatementForValue[T](query: String, values: Seq[Any] = Seq.empty): Future[Option[T]]
     def sendPreparedStatement[T](query: String, values: Seq[Any], deserializer: Row => T): Future[Seq[T]]
     def sendPreparedStatement[T](query: String, deserializer: Row => T): Future[Seq[T]]
+
+    def inTransaction[A](f : Connection => Future[A])(implicit ec : ExecutionContext) : Future[A] = {
+        execute("BEGIN").flatMap { _ =>
+            val p = Promise[A]()
+            f(this).onComplete { r =>
+                execute(if (r.isFailure) "ROLLBACK" else "COMMIT").onComplete {
+                    case Failure(e) if r.isSuccess => p.failure(e)
+                    case _ => p.complete(r)
+                }
+            }
+            p.future
+        }
+    }
 }
